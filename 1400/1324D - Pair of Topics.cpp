5cd214f4f87fe163f16476541f84152d8c002569
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2023-02-21 18:21:58 -0500

    solved 1324D

--- 1400/1324D - Pair of Topics.cpp
+++ 1400/1324D - Pair of Topics.cpp
@@ -4,37 +4,11 @@
  * Problem: 1324D
  * Difficulty Rating: 1400
  * Start Time: 1:19pm
- * End Time:
- * INCOMPLETE SOLUTION, WILL SOLVE LATER ********************
+ * End Time: 6:21pm 2/21/23
  */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-// find first index where the array element is >= x and prev element is < x
-int binarySearch(vector<int> &arr, int l, int r, int &x) {
-    if (r >= l) {
-        int mid = (r + l) / 2;
-
-        // If element is smaller than x
-        if (arr[mid] < x) {
-            // If the next element is greater or equal than x
-            // if (arr[mid + 1] >= x)
-            //     return mid + 1; // index found
-            // Else the value we are looking for is in the right subarray
-            return binarySearch(arr, mid + 1, r, x);
-        }
-        // comes here if greater than or equal to
-        if (arr[mid] >= x) {
-            if (arr[mid - 1] < x)
-                return mid; // index found
-        }
-        // Else look in left subarray
-        return binarySearch(arr, l, mid - 1, x);
-    }
-
-    return -1;
-}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -61,35 +35,24 @@ int main() {
     for (auto x : bs) {
         cout << x << " ";
     }
-
     cout << "\n";
     */
+    // find the element such that when summed together, the sum is greater than 0
 
-    // need to perform binary search to find the another element, which when summed
-    // together, is greater than or equal to 1, meaning the teacher favors it
-
-    int count = 0; // count tracks number of pairs
+    long long count = 0; // count tracks number of pairs
 
     int i;
+
     for (i = 0; i < n-1; i++) {
         if (bs[i] <= 0) {
-            int x = (-1) * (bs[i]) + 1;
-            int j = binarySearch(bs, i + 1, n - 1,x); // j is the index of the element where that element and above are valid
-            // cout << j << "\\ ";
-            if (j != -1) {
-                count += n - j;
-            } else {
-                break;
-            }
+            int j = n - (lower_bound(bs.begin()+i, bs.end(), -bs[i]+1) - bs.begin());
+            count += j;
         } else {
             break;
         }
     }
-    // cout << i << "\n";
-    int l = (n - i); // length
-    if (bs[i] > 0) {
-        count += l*(l-1)/2;
-    }
+    long long k = n-i-1;
+    count += (k*(k+1)/2);
     cout << count;
     return 0;
 }
