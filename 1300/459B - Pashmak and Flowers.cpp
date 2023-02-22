2698763542790e791981bf647281f6d5619ae381
Minsi Hu <101283845+Mimsqueeze@users.noreply.github.com>
2023-02-21 19:03:22 -0500

    Finished 459B

--- 1300/459B - Pashmak and Flowers.cpp
+++ 1300/459B - Pashmak and Flowers.cpp
@@ -4,7 +4,7 @@
  * Problem: 459B
  * Difficulty Rating: 1300
  * Start Time: 7:37PM
- * End Time: incomplete
+ * End Time: 7:03PM 2/21/23
  */
 
 #include <bits/stdc++.h>
@@ -14,5 +14,39 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, b;
+    cin >> n;
+
+    int high = 0;
+    int high_count = 0;
+    int low = INT32_MAX;
+    int low_count = 0;
+
+    while (n--) {
+        cin >> b;
+        if (b == high) {
+            high_count++;
+        }
+        if (b > high) {
+            high = b;
+            high_count = 1;
+        }
+        if (b == low) {
+            low_count++;
+        }
+        if (b < low) {
+            low = b;
+            low_count = 1;
+        }
+    }
+
+    // if high and low are different, then the formula is just their counts
+    // multiplied by each other. Else, the formula is Guass's sum of (count - 1)
+    if (high != low) {
+        cout << high-low << " " << fixed << ((long long) high_count) * low_count;
+    } else {
+        long long count = high_count - 1;
+        cout << "0 " << (count*(count + 1))/2;
+    }
 
 }
